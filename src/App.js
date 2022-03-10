38fc1cb781be1ced42105a8ef8f732baa8d4c680
Iseul Park <lovelyangelapark@gmail.com>
2022-03-11 04:24:35 +0900

    bootstrap-pagination추가 작업

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ function Map() {
   //   console.log("=======")
 
   return (
-    <div className="App">
+    <div class="App">
+      <div class="title">🍴 맛집 리스트 🍴</div>
       {/* 임시 */}
-      {/* <div className="tem">
+      {/* <div class="tem">
       {List.map((item, index) => (
         
-        <div className="tem_1">
+        <div class="tem_1">
             {item.nm}
           </div>
       ))}
@@ -34,7 +35,8 @@ function Map() {
     · user_score - 식당 사용자 평점(ex: 4.1) o
     · review_cnt - 식당 사용자 평점 참여자 수 o
     · favorites_cnt - 식당 좋아요 수 o
-        "nm":"송화산시도삭면",
+    ------------------------------------------------
+    "nm":"송화산시도삭면",
     "branch":"2호점",
     "image":"https:\/\/s3-ap-northeast-1.amazonaws.com\/dcreviewsresized\/pre_20211216024924764_photo_i0OmWUtveemW.jpg","area":"건대",
     "distance":"884m",
@@ -45,33 +47,34 @@ function Map() {
     "favorites_cnt":137
       */}
       {/* Json형식의 배열데이터를 map함수를 이용해 반복처리 */}
-      {List.map((item, index) => (
-        <div className="ListBox">
-          <ul className="ListBox_ul">
+      {List.map((item) => (
+        <div class="ListBox">
+          <ul class="ListBox_ul">
             {/* mapping 예정 */}
-            <li className="ListBox_li">
-              <div className="ListBox_li_imgBox"><img
+            <li class="ListBox_li">
+              <div class="ListBox_li_imgBox"><img
                 src={item.image}
                 alt={item.image}
                 layout="responsive"
                 objectFit="cover" //써야됨 or none
                 width={100}
                 height={100}
+                style={{borderRadius:"20px"}}
               /></div>
-              <div className="ListBox_li_contents">
-                <div className="ListBox_li_contents1">
-                  <div className="ListBox_li_contents1_nm_area">
-                    <span className="ListBox_li_contents1_nm">{item.nm}</span>
-                    <span className="ListBox_li_contents1_area">{item.area} | {item.distance}</span>
+              <div class="ListBox_li_contents">
+                <div class="ListBox_li_contents1">
+                  <div class="ListBox_li_contents1_nm_area">
+                    <span class="ListBox_li_contents1_nm">{item.nm}</span>
+                    <span class="ListBox_li_contents1_area">{item.area} | {item.distance}</span>
                   </div>
-                  <span className="ListBox_li_contents1_category">{item.category}</span>
+                  <span class="ListBox_li_contents1_category">{item.category}</span>
                   {/* 각인덱스 앞 #추가 */}
-                  <span className="ListBox_li_contents1_keyword">#{item.keyword.join(" #")}</span>
+                  <span class="ListBox_li_contents1_keyword">#{item.keyword.join(" #")}</span>
                 </div>
-                <div className="ListBox_li_contents2">
-                  <span className="ListBox_li_contents2_score">{item.score}<span className="ListBox_li_contents2_score_">점</span></span>
-                  <span className="ListBox_li_contents2_review_cnt">⭐ {item.user_score} ({item.review_cnt})</span>
-                  <span className="ListBox_li_contents2_favorites_cnt">💜 {item.favorites_cnt}</span>
+                <div class="ListBox_li_contents2">
+                  <span class="ListBox_li_contents2_score">{item.score}<span class="ListBox_li_contents2_score_">점</span></span>
+                  <span class="ListBox_li_contents2_review_cnt">⭐ {item.user_score} ({item.review_cnt})</span>
+                  <span class="ListBox_li_contents2_favorites_cnt">💜 {item.favorites_cnt}</span>
                 </div>
               </div>
             </li>
@@ -80,7 +83,22 @@ function Map() {
 
         </div>
       ))}
-
+      {/* 부트스트랩 pagination추가 */}
+      <div class="pagination">
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+        </nav>
+        </div>
     </div>
   );
 }
